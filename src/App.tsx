6a8f9e78f698a712a7485be190d5c0494a5f7815
Mauricio Erazo <antonierazo13@hotmail.com>
2025-02-04 17:10:33 +0100

    styles: add responsive container layout and update media

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,12 @@ import styles from './App.module.css';
 function App() {
   return (
     <>
-      <h1 className={styles.title}>Hello</h1>
+      <h1 className={styles.title}>Weather Page</h1>
+
+      <div className={styles.container}>
+        <p>Place</p>
+        <p>Info</p>
+      </div>
     </>
   );
 }
